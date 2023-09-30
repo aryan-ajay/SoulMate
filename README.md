2c6f1455a49ad5509d348849c015ff1dc48faa52
Ajay <timecupon@gmail.com>
2023-09-30 21:13:27 +0530

    home page created

--- README.md
+++
@@ -1,2 +0,0 @@
-# SoulMate
-A college based project.
--- app.js
+++ app.js
@@ -1,11 +1,67 @@
-const sign_in_btn = document.querySelector("#sign-in-btn");
-const sign_up_btn = document.querySelector("#sign-up-btn");
-const container = document.querySelector(".container");
+let likeCount = 0; // Initial like count
 
-sign_up_btn.addEventListener("click", () => {
-  container.classList.add("sign-up-mode");
+document.querySelectorAll(".user_likes").forEach((likeButton) => {
+  likeButton.addEventListener("click", () => {
+    const likesCounts = document.querySelector(".likes_counts");
+    likesCounts.style.display =
+      likesCounts.style.display === "none" || likesCounts.style.display === ""
+        ? "flex"
+        : "none";
+
+    if (likesCounts.style.display === "flex") {
+      likeCount += 1;
+    } else {
+      likeCount -= 1;
+    }
+
+    document.getElementById("likeCount").textContent = likeCount;
+  });
 });
 
-sign_in_btn.addEventListener("click", () => {
-  container.classList.remove("sign-up-mode");
-});
+// comment area ******************************************************
+
+function togglePostButton() {
+  const commentInput = document.getElementById("commentInput").value.trim();
+  const postButton = document.getElementById("postButton");
+
+  if (commentInput !== "") {
+    postButton.style.display = "block"; // Show the button if there's input
+  } else {
+    postButton.style.display = "none"; // Hide the button if input is empty
+  }
+}
+
+function addCommentOnEnter(event) {
+  if (event.key === "Enter") {
+    event.preventDefault(); // Prevent the default behavior of the Enter key
+    addComment(); 
+  }
+}
+
+function addComment() {
+  const commentInput = document.getElementById("commentInput").value.trim();
+  if (commentInput !== "") {
+    const commentContainer = document.getElementById("commentContainer");
+    const newComment = document.createElement("div");
+    newComment.classList.add("comment");
+    newComment.innerHTML = `
+        <span class="comment-text">${commentInput}</span>
+      `;
+    commentContainer.appendChild(newComment);
+    document.getElementById("commentInput").value = "";
+    document.getElementById("postButton").style.display = "none"; // Hide the button after posting comment
+
+    // Check if the comment container is full and show "Show more" button
+    if (commentContainer.scrollHeight > 200) {
+      const showMoreButton = document.getElementById("showMoreButton");
+      showMoreButton.style.display = "block";
+    }
+  }
+}
+
+function showMoreComments() {
+  const commentContainer = document.getElementById("commentContainer");
+  commentContainer.style.maxHeight = "none"; // Remove max height to show all comments
+  const showMoreButton = document.getElementById("showMoreButton");
+  showMoreButton.style.display = "none"; // Hide the "Show more" button
+}
